cfe8118f00123823dce9cca062dab7a40de76b49
Chenxia Han <chenxia.han@whu.edu.cn>
2019-05-30 15:32:54 +0800

    refactor retinanet (#106)
    * refactor
    
    * fix
    
    * update metric
    
    * fix
    
    * fix
    
    * simplify metric

--- models/retinanet/metric.py
+++ models/retinanet/metric.py
@@ -1,25 +1,39 @@
 import numpy as np
 import mxnet as mx
 
+
 class FGAccMetric(mx.metric.EvalMetric):
-    def __init__(self, name, output_names, label_names):
+    def __init__(self, name, output_names, label_names, threshold=0):
         super(FGAccMetric, self).__init__(name, output_names, label_names)
+        self.thr = threshold
 
     def update(self, labels, preds):
-        pred = preds[0]
-        label = labels[0]
+        if len(preds) == 1 and len(labels) == 1:
+            pred = preds[0]
+            label = labels[0]
+        elif len(preds) == 2:
+            pred = preds[0]
+            label = preds[1]
+        else:
+            raise Exception(
+                "unknown loss output: len(preds): {}, len(labels): {}".format(
+                    len(preds), len(labels)
+                )
+            )
 
-        # pred (b, p, c) , c is 0 ~ CLASSNUM-1
-        pred_label = mx.nd.argmax(pred,axis=2)
-        # pred_label = mx.nd.argmax_channel(pred)
-        pred_label = pred_label.asnumpy().astype('int32')
-        # label (b, p)
         label = label.asnumpy().astype('int32')
-
         keep_inds = np.where(label >= 1)
+
+        # treat as foreground if score larger than threshold
+        # select class with maximum score as prediction
+        pred_score = pred.max(axis=-1)
+        pred_label = pred.argmax(axis=-1) + 1
+        pred_label *= pred_score > self.thr
+
+        pred_label = pred_label.asnumpy().astype('int32')
+
         pred_label = pred_label[keep_inds]
-        # label is 1 ~ CLASSNUM , so label = label - 1
-        label = label[keep_inds] - 1
+        label = label[keep_inds]
 
         self.sum_metric += np.sum(pred_label.flat == label.flat)
         self.num_inst += len(pred_label.flat)
