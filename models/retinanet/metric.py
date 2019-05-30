28dd1e231f1842907b2de37814eb6511334464be
Chenxia Han <chenxia.han@whu.edu.cn>
2019-05-30 14:16:07 +0800

    simplify metric

--- models/retinanet/metric.py
+++ models/retinanet/metric.py
@@ -3,8 +3,9 @@ import mxnet as mx
 
 
 class FGAccMetric(mx.metric.EvalMetric):
-    def __init__(self, name, output_names, label_names):
+    def __init__(self, name, output_names, label_names, threshold=0):
         super(FGAccMetric, self).__init__(name, output_names, label_names)
+        self.thr = threshold
 
     def update(self, labels, preds):
         if len(preds) == 1 and len(labels) == 1:
@@ -20,23 +21,19 @@ class FGAccMetric(mx.metric.EvalMetric):
                 )
             )
 
-        # label (b, p)
         label = label.asnumpy().astype('int32')
         keep_inds = np.where(label >= 1)
 
-        if pred.shape[-1] == 1:
-            # treat as foreground if score larger than 0.05
-            pred_label = pred[:, :, 0] > 0.05
-            label = label[keep_inds]
-        else:
-            # pred (b, p, c) , c is 0 ~ CLASSNUM-1
-            pred_label = mx.nd.argmax(pred, axis=2)
-            # label is 1 ~ CLASSNUM , so label = label - 1
-            label = label[keep_inds] - 1
+        # treat as foreground if score larger than threshold
+        # select class with maximum score as prediction
+        pred_score = pred.max(axis=-1)
+        pred_label = pred.argmax(axis=-1) + 1
+        pred_label *= pred_score > self.thr
 
         pred_label = pred_label.asnumpy().astype('int32')
 
         pred_label = pred_label[keep_inds]
+        label = label[keep_inds]
 
         self.sum_metric += np.sum(pred_label.flat == label.flat)
         self.num_inst += len(pred_label.flat)
