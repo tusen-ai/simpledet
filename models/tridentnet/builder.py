84b84707da17b844ec9a6e44c46115215494a580
Yuntao Chen <chenyuntao08@gmail.com>
2019-06-14 16:06:45 +0800

    add tridentnet with resnet_v1 backbone
    (cherry picked from commit a48177a3fbda72025feeb3095c95246331406fe4)

--- models/tridentnet/builder.py
+++ models/tridentnet/builder.py
@@ -7,6 +7,7 @@ import mxnext as X
 from symbol.builder import RpnHead, Backbone, RoiExtractor
 from models.tridentnet.resnet_v2 import TridentResNetV2Builder
 from models.tridentnet.resnet_v1b import TridentResNetV1bBuilder
+from models.tridentnet.resnet_v1 import TridentResNetV1Builder
 from utils.patch_config import patch_config_as_nothrow
 
 
@@ -450,6 +451,22 @@ class TridentResNetV1bC4(Backbone):
         super().__init__(pBackbone)
         p = self.p
         b = TridentResNetV1bBuilder()
+        self.symbol = b.get_backbone(p.depth, "c4", p.normalizer, p.fp16,
+                                     p.num_branch, p.branch_dilates, p.branch_ids,
+                                     p.branch_bn_shared, p.branch_conv_shared, p.branch_deform)
+
+    def get_rpn_feature(self):
+        return self.symbol
+
+    def get_rcnn_feature(self):
+        return self.symbol
+
+
+class TridentResNetV1C4(Backbone):
+    def __init__(self, pBackbone):
+        super().__init__(pBackbone)
+        p = self.p
+        b = TridentResNetV1Builder()
         self.symbol = b.get_backbone(p.depth, "c4", p.normalizer, p.fp16,
                                      p.num_branch, p.branch_dilates, p.branch_ids,
                                      p.branch_bn_shared, p.branch_conv_shared, p.branch_deform)
