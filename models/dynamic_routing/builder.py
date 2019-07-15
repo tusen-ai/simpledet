8c4ad5a3e12f1be8b846e520b485966f68493461
Yuntao Chen <chenyuntao08@gmail.com>
2019-07-15 13:27:02 +0800

    init dcn

--- models/dynamic_routing/builder.py
+++ models/dynamic_routing/builder.py
@@ -78,3 +78,66 @@ class DRResNetC4(Backbone):
 
     def get_rcnn_feature(self):
         return self.symbol
+
+
+def dcn_resnet_unit(input, name, filter, stride, dilate, proj, norm):
+    conv1 = conv(input, name=name + "_conv1", filter=filter // 4)
+    bn1 = norm(conv1, name=name + "_bn1")
+    relu1 = relu(bn1, name=name + "_relu1")
+
+    # conv2 filter router
+    conv2_offset = conv(relu1, name=name + "_conv2_offset", filter=72, kernel=3, stride=stride, dilate=dilate)
+    conv2 = mx.sym.contrib.DeformableConvolution(relu1, conv2_offset, kernel=(3, 3),
+        stride=(stride, stride), dilate=(dilate, dilate), pad=(1, 1), num_filter=filter // 4,
+        num_deformable_group=4, no_bias=True, name=name + "_conv2")
+    bn2 = norm(conv2, name=name + "_bn2")
+    relu2 = relu(bn2, name=name + "_relu2")
+
+    conv3 = conv(relu2, name=name + "_conv3", filter=filter)
+    bn3 = norm(conv3, name=name + "_bn3")
+
+    if proj:
+        shortcut = conv(input, name=name + "_sc", filter=filter, stride=stride)
+        shortcut = norm(shortcut, name=name + "_sc_bn")
+    else:
+        shortcut = input
+
+    eltwise = add(bn3, shortcut, name=name + "_plus")
+
+    return relu(eltwise, name=name + "_relu")
+
+def dcn_resnet_stage(data, name, num_block, filter, stride, dilate, norm):
+    s, d = stride, dilate
+
+    data = resnet_unit(data, "{}_unit1".format(name), filter, s, d, True, norm)
+    for i in range(2, num_block + 1 - 3):
+        data = resnet_unit(data, "{}_unit{}".format(name, i), filter, 1, d, False, norm)
+    for i in range(num_block + 1 - 3, num_block + 1):
+        data = dcn_resnet_unit(data, "{}_unit{}".format(name, i), filter, 1, d, False, norm)
+
+    return data
+
+
+class DCNResNetC4(Backbone):
+    def __init__(self, pBackbone):
+        super().__init__(pBackbone)
+        p = self.p
+
+        import mxnext.backbone.resnet_v1b_helper as helper
+        num_c2, num_c3, num_c4, _ = helper.depth_config[p.depth]
+
+        data = X.var("data")
+        if p.fp16:
+            data = data.astype("float16")
+        c1 = helper.resnet_c1(data, p.normalizer)
+        c2 = helper.resnet_c2(c1, num_c2, 1, 1, p.normalizer)
+        c3 = helper.resnet_c3(c2, num_c3, 2, 1, p.normalizer)
+        c4 = dcn_resnet_stage(c3, "stage3", num_c4, 1024, 2, 1, p.normalizer)
+
+        self.symbol = c4
+
+    def get_rpn_feature(self):
+        return self.symbol
+
+    def get_rcnn_feature(self):
+        return self.symbol
