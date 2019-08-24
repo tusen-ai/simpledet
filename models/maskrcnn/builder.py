52a41ef41bfa486e3c018e55574454b97dc029a7
Yuntao Chen <chenyuntao08@gmail.com>
2019-08-24 08:40:47 +0800

    update

--- models/maskrcnn/builder.py
+++ models/maskrcnn/builder.py
@@ -308,7 +308,7 @@ class MaskFasterRcnn4ConvHead(MaskFasterRcnnHead):
         p = self.pMask
         if p.normalizer.__name__ == "fix_bn":
             pass
-        elif p.normalizer.__name__ in ["sync_bn", "gn"]:
+        elif p.normalizer.__name__ in ["sync_bn", "local_bn", "gn", "dummy"]:
             sym = p.normalizer(sym)
         else:
             raise NotImplementedError("Unsupported normalizer: {}".format(p.normalizer.__name__))
