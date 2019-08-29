57bc1678dd7f1f8536fe133f78d9a5c75cd28f94
Yuntao Chen <chenyuntao08@gmail.com>
2019-08-29 21:16:29 +0800

    add support for tensorboard

--- models/maskrcnn/metric.py
+++ models/maskrcnn/metric.py
@@ -1,9 +1,12 @@
 import numpy as np
 import mxnet as mx
 
-class SigmoidCELossMetric(mx.metric.EvalMetric):
-    def __init__(self, name, output_names, label_names):
-        super().__init__(name, output_names, label_names)
+from core.detection_metric import EvalMetricWithSummary
+
+
+class SigmoidCELossMetric(EvalMetricWithSummary):
+    def __init__(self, name, output_names, label_names, **kwargs):
+        super().__init__(name, output_names, label_names, **kwargs)
 
     def update(self, labels, preds):
         self.sum_metric += preds[0].mean().asscalar()
