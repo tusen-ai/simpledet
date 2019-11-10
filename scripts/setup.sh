3cc64e4ec7024b5d5547838cefc682dec91406cf
Yuntao Chen <RogerChern@users.noreply.github.com>
2019-11-10 21:17:03 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,19 +4,24 @@
 # install dependency
 sudo apt update && sudo apt install -y git wget make python3-dev libglib2.0-0 libsm6 libxext6 libxrender-dev unzip
 
-# install python dependency
-pip3 install 'matplotlib<3.1' opencv-python pytz --user
+# create conda env
+conda create -n simpledet python=3.7
+conda activate simpledet
 
-# download and intall pre-built wheel for CUDA 10.0
-# check INSTALL.md for wheels for other CUDA version
-wget https://bit.ly/2jRGqdc -O mxnet_cu100-1.6.0b20190820-py2.py3-none-manylinux1_x86_64.whl
-pip3 install mxnet_cu100-1.6.0b20190820-py2.py3-none-manylinux1_x86_64.whl --user
+# fetch CUDA environment
+conda install cudatoolkit=10.1
+
+# install python dependency
+pip install 'matplotlib<3.1' opencv-python pytz
+
+# download and intall pre-built wheel for CUDA 10.1
+pip install https://1dv.alarge.space/mxnet_cu101-1.6.0b20190820-py2.py3-none-manylinux1_x86_64.whl
 
 # install pycocotools
-pip3 install 'git+https://github.com/RogerChern/cocoapi.git#subdirectory=PythonAPI' --user
+pip install 'git+https://github.com/RogerChern/cocoapi.git#subdirectory=PythonAPI'
 
 # install mxnext, a wrapper around MXNet symbolic API
-pip3 install 'git+https://github.com/RogerChern/mxnext#egg=mxnext' --user
+pip install 'git+https://github.com/RogerChern/mxnext#egg=mxnext'
 
 # get simpledet
 git clone https://github.com/tusimple/simpledet
@@ -39,6 +44,6 @@ unzip data/src/test2017.zip -d data/coco/images
 unzip data/src/annotations_trainval2017.zip -d data/coco
 unzip data/src/image_info_test2017.zip -d data/coco
 
-python3 utils/create_coco_roidb.py --dataset coco --dataset-split train2017
-python3 utils/create_coco_roidb.py --dataset coco --dataset-split val2017
-python3 utils/create_coco_roidb.py --dataset coco --dataset-split test-dev2017
+python utils/create_coco_roidb.py --dataset coco --dataset-split train2017
+python utils/create_coco_roidb.py --dataset coco --dataset-split val2017
+python utils/create_coco_roidb.py --dataset coco --dataset-split test-dev2017
