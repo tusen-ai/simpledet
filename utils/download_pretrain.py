e27c1e7cfb7fabd092021692390906bf5be42490
Yuntao Chen <chenyuntao08@gmail.com>
2019-11-10 17:00:51 +0800

    download pretrain if missing

---
+++ utils/download_pretrain.py
@@ -0,0 +1,22 @@
+import os
+import urllib.request
+
+
+def report(block_count, block_size, content_size):
+    if block_count % (content_size // block_size // 5) == 1:
+        print("Downloaded %.1f/100" % (block_size * block_count / content_size * 100))
+
+
+def download(prefix, epoch):
+    base_name = prefix.replace("pretrain_model/", "") + "-%04d.params" % epoch
+    save_name = "%s-%04d.params" % (prefix, epoch)
+    base_url = os.environ.get("SIMPLEDET_BASE_URL", "https://1dv.alarge.space/")
+    full_url = base_url + base_name
+
+    try:
+        print("Downloading %s from %s" % (save_name, full_url))
+        urllib.request.urlretrieve(full_url, save_name, report)
+    except Exception as e:
+        print("Fail to download %s. You can mannually download it from %s and put it to %s" % (base_name, full_url, save_name))
+        os.remove(save_name)
+        raise e
